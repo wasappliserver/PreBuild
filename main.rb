38a353231969276e147d11731129513a1311cf27
wasappliserver <wasappliserver@wasapp.li>
2014-05-12 17:03:14 -0400

    did everything again

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
+require_relative "pre_build.rb"
 require 'sqlite3'
 require 'rubygems'
 require 'fileutils'
 require 'optparse'
-require_relative "pre_build.rb"
 
 #get the parameter which is the name of the app
 options = {}
@@ -13,51 +13,36 @@ OptionParser.new do |opts|
   end
 end.parse!
 
+init
+
 #gets all the values from the DB for numbers colors and strings, stores them in array for each
 db = SQLite3::Database.new("/Users/wasappliserver/RubymineProjects/ConstantPicker/db/development.sqlite3")
-rows_number = db.execute("SELECT title, value FROM numbers n JOIN apps a where a.name = '#{options[:name]}'")
-rows_string = db.execute("SELECT title, value FROM at_strings n JOIN apps a where a.name = '#{options[:name]}'")
-rows_color = db.execute("SELECT title, value_coul FROM couleurs n JOIN apps a where a.name = '#{options[:name]}'")
+@rows_number = db.execute("SELECT title, value FROM numbers n JOIN apps a where a.name = '#{options[:name]}'")
+@rows_string = db.execute("SELECT title, value FROM at_strings n JOIN apps a where a.name = '#{options[:name]}'")
+@rows_color = db.execute("SELECT title, value_coul FROM couleurs n JOIN apps a where a.name = '#{options[:name]}'")
 
 #log display
-display_rows rows_number
-display_rows rows_string
-display_rows rows_color
 
-path="/Users/wasappliserver/.jenkins/jobs/#{options[:name]}/workspace/#{options[:name]}/Constants.h"
-text = Array.new
-patt_strings="/(\S*)\s(\S*)\s*(@\"\S*\")/"
-patt_colors="/\S+\s(\S*)\s\[(\S*)\s(.*):([\w]*)\]/"
-patt_numbers= "/(\S*)\s(\S*)\s(\d+)/"
+#display_rows @rows_number
+#display_rows @rows_string
+#display_rows @rows_color
 
+path="/Users/wasappliserver/Downloads/Constants.h"
+
+@marker=0
 #If the file exist
 if (File.exist?(path))
 #open it
-  File.open(path) do |file|
-#read lines
-    text = file.readlines
-#loop with lines
-    for i in 0..text.count-1
+  File.open(path, 'r+') do |file|
+#read lines, fill the tabs
+    @lines = file.readlines
 
-      #if pattern =~ '#define'
-      if (text[i][/#define/])
-
-        #function line =~ pattern color/string/number, puts 'color' or 'number' or 'string' int @line_type, return true or false
-        if (isLine(text[i]))
-
-          #if line match a row (function return boolean)
-          if (compareLine(text[i]))
-            #modify the value
-            modifiyLine(text[i])
-
-            #else dont touch
-          end
-        end #endif
-
-      end #endfor
-
-    end #endif
-
-  end #endif
-
+    #dabord les rows numbers
+    @rows_number.each do |row|
+      if(compareLinesN row == false)
+        #ADD THE ROWWWW
+        puts "ADD NEW ROW"
+      end
+    end
+  end
 end
--- pre_build.rb
+++ pre_build.rb
@@ -1,20 +1,49 @@
+def init
+  @rows_color= Array.new
+  @rows_string = Array.new
+  @rows_number = Array.new
 
-#displaying rows to debug/log
-def display_rows rows
-  rows.each do |row|
-    puts row.to_s
+end
+
+def compareLinesN row
+
+  for i in 0..@lines.count-1
+    if (@lines[i]=~ /(#\S*)\s(\S*)\s(\d+)/m)
+      if (compareLineN(i, row))
+        return true
+      else
+        return false
+      end
+    end
   end
-  puts ''
 end
 
-def isLine str
-
+def compareLineN i, row
+  data = @lines[i].scan(/(#\S*)\s(\S*)\s(\d+)/)
+  if (data[0][1]==row[0])
+    puts "MATCH NUMBER data|||row ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    puts "ROW" + row.to_s
+    @lines[i] = "#define #{row[0]} #{row[1]}"
+    puts @lines[i].to_s
+    return true
+  end
+  return false
 end
 
-def modifyLine str
+def compareLineS i, row
+  data = @lines[i].scan(/(\S*)\s(\S*)\s*(@"\S*")/)
 
+  if (data[0][1]==row[0])
+    puts "MATCH ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    return true
+  end
 end
 
-def compareLine str
-
+def compareLineC i, row
+  data = @lines[i].scan(/\S+\s(\S*)\s\[(\S*)\s(.*):([\w]*)\]/)
+  if (data[0][1]==row[0])
+    puts "MATCH ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    return true
+  end
 end
+
