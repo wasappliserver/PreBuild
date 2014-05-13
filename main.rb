3b850b26903e2fddaf013f250a5cc73534ecf558
wasappliserver <wasappliserver@wasapp.li>
2014-05-13 11:00:36 -0400

    FINAL RELEASE

--- main.rb
+++ main.rb
@@ -15,11 +15,13 @@ end.parse!
 
 init
 
+
 #gets all the values from the DB for numbers colors and strings, stores them in array for each
 db = SQLite3::Database.new("/Users/wasappliserver/RubymineProjects/ConstantPicker/db/development.sqlite3")
-@rows_number = db.execute("SELECT title, value FROM numbers n JOIN apps a where a.name = '#{options[:name]}'")
-@rows_string = db.execute("SELECT title, value FROM at_strings n JOIN apps a where a.name = '#{options[:name]}'")
-@rows_color = db.execute("SELECT title, value_coul FROM couleurs n JOIN apps a where a.name = '#{options[:name]}'")
+id = db.execute("Select id from apps where name = ?", options[:name])
+@rows_number = db.execute("SELECT title, value FROM numbers WHERE app_id = ?", id)
+@rows_string = db.execute("SELECT title, value FROM at_strings  WHERE app_id = ?", id)
+@rows_color = db.execute("SELECT title, value_coul, coul_type FROM couleurs  WHERE app_id = ?", id)
 
 #log display
 
@@ -35,14 +37,45 @@ if (File.exist?(path))
 #open it
   File.open(path, 'r+') do |file|
 #read lines, fill the tabs
-    @lines = file.readlines
+    @lines_init = file.readlines
+    @lines_final = @lines_init.dup
 
-    #dabord les rows numbers
+
     @rows_number.each do |row|
-      if(compareLinesN row == false)
+      puts "ROW ==>" + row.to_s
+
+
+      if (compareLinesN(row)== false)
         #ADD THE ROWWWW
-        puts "ADD NEW ROW"
+        puts
+        puts "ADD NEW ROW NUMBER"
+        addRowNumber row
       end
     end
+
+    @rows_color.each do |row|
+      if (compareLinesC(row)== false)
+        #ADD THE ROWWWW
+        puts
+        puts "ADD NEW ROW COLOR"
+        addRowColor row
+      end
+    end
+
+    @rows_string.each do |row|
+      if (compareLinesS(row)== false)
+        #ADD THE ROWWWW
+        puts
+        puts "ADD NEW ROW String"
+        addRowString row
+      end
+    end
+
   end
 end
+File.open(path, 'w') do |file|
+  @lines_final.each do |line|
+    file << line.to_s
+  end
+end
+
--- pre_build.rb
+++ pre_build.rb
@@ -5,45 +5,139 @@ def init
 
 end
 
+#################################################################
+
+########## N U M B E R S ##############
 def compareLinesN row
 
-  for i in 0..@lines.count-1
-    if (@lines[i]=~ /(#\S*)\s(\S*)\s(\d+)/m)
-      if (compareLineN(i, row))
+  for i in 0..@lines_init.count-1
+    #puts "LIGNE REGARDEE =>" + @lines_init[i].to_s
+    if (@lines_init[i]=~ /(#\S*)\s(\S*)\s(\d+)/m)
+      if (compareOneLineN(i, row))
         return true
-      else
-        return false
       end
     end
   end
+  return false
 end
 
-def compareLineN i, row
-  data = @lines[i].scan(/(#\S*)\s(\S*)\s(\d+)/)
+
+def compareOneLineN i, row
+  data = @lines_init[i].scan(/(#\S*)\s(\S*)\s(\d+)/)
+  puts "NUMBER data|||row ===>" + data[0][1].to_s + "|||" + row[0].to_s
+
   if (data[0][1]==row[0])
-    puts "MATCH NUMBER data|||row ===>" + data[0][1].to_s + "|||" +row[0].to_s
-    puts "ROW" + row.to_s
-    @lines[i] = "#define #{row[0]} #{row[1]}"
-    puts @lines[i].to_s
+    puts "MATCH " #NUMBER data|||row ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    puts "LINE BEFORE" + @lines_final[i].to_s
+    @lines_final[i] = "#define #{row[0]} #{row[1]}\n"
+    puts "LINE AFTER" + @lines_final[i].to_s
+    puts
     return true
   end
   return false
 end
 
-def compareLineS i, row
-  data = @lines[i].scan(/(\S*)\s(\S*)\s*(@"\S*")/)
+##############################################################################
+
+########## S T R I N G S ##############
+def compareLinesS row
+
+  for i in 0..@lines_init.count-1
+    puts "LIGNE REGARDEE =>" + @lines_init[i].to_s
+    if (@lines_init[i]=~ /(\S*)\s(\S*)\s*(@"\S*")/m)
+      if (compareOneLineS(i, row))
+        return true
+      end
+    end
+  end
+  return false
+end
+
+def compareOneLineS i, row
+  data = @lines_init[i].scan(/(\S*)\s(\S*)\s*(@"\S*")/)
+  puts "String data|||row ===>" + data[0][1].to_s + "|||" + row[0].to_s
 
   if (data[0][1]==row[0])
-    puts "MATCH ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    puts "MATCH " #NUMBER data|||row ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    puts "LINE BEFORE" + @lines_final[i].to_s
+    @lines_final[i] = "#define #{row[0]} @\"#{row[1]}\"\n"
+    puts "LINE AFTER" + @lines_final[i].to_s
+    puts
     return true
+  end
+  return false
+end
+
+##############################################################################
+
+########## C O L O R S ##############
+def compareLinesC row
+
+  for i in 0..@lines_init.count-1
+    #puts "LIGNE REGARDEE =>" + @lines_init[i].to_s
+    if (@lines_init[i]=~ /\S+\s(\S*)\s\[(\S*)\s(.*):([\w]*)\]/m)
+      if (compareOneLineC(i, row))
+        return true
+      end
+    end
+  end
+  return false
+end
+
+def compareOneLineC i, row
+  data = @lines_init[i].scan(/\S+\s(\S*)\s\[(\S*)\s(.*):([\w]*)\]/)
+  puts "Color data|||row ===>" + data[0][0].to_s + "|||" + row[0].to_s
+
+  if (data[0][0]==row[0])
+    puts "MATCH " #NUMBER data|||row ===>" + data[0][1].to_s + "|||" +row[0].to_s
+    puts "LINE BEFORE" + @lines_final[i].to_s
+    @lines_final[i] = "#define #{row[0]} [#{row[2]} colorWithHexa:#{row[1]}]\n"
+    puts "LINE AFTER" + @lines_final[i].to_s
+    puts
+    return true
+  end
+  return false
+end
+
+####################################
+
+########## A D D  R O W  N U M B E R ##############
+def addRowNumber row
+
+  #on doit atteindre le #endif et ecrire avant
+  for i in 0..@lines_init.count-1
+    if (@lines_final[i] =~ /#endif/)
+      @lines_final.insert(i-1, "#define #{row[0]} #{row[1]}\n")
+      puts "line i-1 after" + @lines_final[i-1].to_s
+    end
   end
 end
 
-def compareLineC i, row
-  data = @lines[i].scan(/\S+\s(\S*)\s\[(\S*)\s(.*):([\w]*)\]/)
-  if (data[0][1]==row[0])
-    puts "MATCH ===>" + data[0][1].to_s + "|||" +row[0].to_s
-    return true
+######################################
+
+########## A D D  R O W  C O L O R ##############
+def addRowColor row
+
+  #on doit atteindre le #endif et ecrire avant
+  for i in 0..@lines_final.count-1
+    if (@lines_final[i] =~ /#endif/)
+      @lines_final.insert(i-1, "#define #{row[0]} [#{row[2]} colorWithHexa:#{row[1]}]\n")
+      puts "line i-1 after" + @lines_final[i-1].to_s
+    end
   end
 end
 
+######################################
+
+########## A D D  R O W  S T R I N G ##############
+def addRowString row
+
+  #on doit atteindre le #endif et ecrire avant
+  for i in 0..@lines_final.count-1
+    if (@lines_final[i] =~ /#endif/)
+      @lines_final.insert(i-1, "#define #{row[0]} @\"#{row[1]}\"\n")
+      puts "line i-1 after" + @lines_final[i-1].to_s
+    end
+  end
+end
+######################################
